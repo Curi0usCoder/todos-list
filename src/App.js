4ce591542442eca5b648995229cdaff341ee00ef
Sarath <136047799+Curi0usCoder@users.noreply.github.com>
2024-02-14 20:05:41 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
 import logo from './logo.svg';
 import './App.css';
+import Header from './myComponents/Header';
+import {Footer} from './myComponents/Footer';
+import {Todos} from './myComponents/Todos';
+import { useState } from 'react';
+import { AddTodos } from './myComponents/AddTodos';
 
 function App() {
+
+const onDelete = (todo)=>{
+  console.log("Iam onDelete function", todo);
+  setTodos(todos.filter((e)=>{
+    return e!==todo;
+  }))
+}
+
+const addTodo = (title, desc)=>{
+  console.log("I am adding this todo", title, desc)
+  let sno=1;
+  if(!todos.length==0){
+    sno = todos[todos.length-1].sno+1;
+  }
+  
+  const myTodo = {
+    sno: sno,
+    title: title,
+    desc: desc,
+  }
+  setTodos([...todos, myTodo]);
+  console.log(myTodo);
+}
+
+const [todos, setTodos] =useState([
+  
+]
+); 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Header title = "My Todos List" searchbar = {false}/>
+    <AddTodos addTodo={addTodo} />
+    <Todos todos={todos} onDelete={onDelete}/>
+    <Footer/>
+    </>
   );
 }
 
